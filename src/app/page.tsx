e8da71dd2850414d11650bf20cf94529a9bbf93c
Aryan Puri Goswami <111564126+Aryan-the-programmer@users.noreply.github.com>
2026-08-23 01:37:16 +0530

    Added sponsor section (#9)
    Co-authored-by: Vaibhav Sharma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { ExperienceShell } from '@/components/intro/ExperienceShell';
 import { GlitchverseHero } from '@/components/hero/GlitchverseHero';
+import { SponsorSection } from "@/components/sponsor-section/SponsorSection";
 import { TimelineRoad } from '@/components/timeline/TimelineRoad';
 import { EventSections } from '@/components/sections/EventSections';
 import { BackgroundVideo } from '@/components/layout/BackgroundVideo';
@@ -12,15 +13,15 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <>
-      <BackgroundVideo />
-      <ExperienceShell>
-        <div className='flex min-h-screen flex-col bg-transparent'>
-          <GlitchverseHero />
-          <TimelineRoad />
-          <EventSections />
-        </div>
-      </ExperienceShell>
-    </>
+    
+    <ExperienceShell>
+      <div className='flex min-h-screen flex-col bg-black'>
+        <GlitchverseHero />
+        <SponsorSection />
+        <TimelineRoad />
+        <EventSections />
+      </div>
+    </ExperienceShell>
+
   );
 }
