8c3a3a66c98a0de208b6217a0caa6a9f9c014211
vaibhav9526 <vaibhavsharma040506@gmail.com>
2026-08-23 20:08:37 +0530

    release: publish CodeUtsava v1.0.0 experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import { ExperienceShell } from '@/components/intro/ExperienceShell';
 import { GlitchverseHero } from '@/components/hero/GlitchverseHero';
 import { SponsorSection } from "@/components/sponsor-section/SponsorSection";
 import { TimelineRoad } from '@/components/timeline/TimelineRoad';
+import { GuidelinesSection } from '@/components/sections/guidelines-section';
+import { PrizesSection } from '@/components/sections/prizes-section';
+import { GraphAnalytics } from '@/components/analytics/GraphAnalytics';
 import { EventSections } from '@/components/sections/EventSections';
 import { AboutSection } from '@/components/sections/about-section';
 import { BackgroundVideo } from '@/components/layout/BackgroundVideo';
@@ -14,17 +17,18 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <>
+    <ExperienceShell>
       <BackgroundVideo />
-      <ExperienceShell>
-        <div className='flex min-h-screen flex-col bg-transparent'>
-          <GlitchverseHero />
-          <AboutSection />
-          <SponsorSection />
-          <TimelineRoad />
-          <EventSections />
-        </div>
-      </ExperienceShell>
-    </>
+      <div className='flex min-h-screen flex-col bg-transparent'>
+        <GlitchverseHero />
+        <AboutSection />
+        <SponsorSection />
+        <TimelineRoad />
+        <GuidelinesSection />
+        <PrizesSection />
+        <GraphAnalytics />
+        <EventSections />
+      </div>
+    </ExperienceShell>
   );
 }
